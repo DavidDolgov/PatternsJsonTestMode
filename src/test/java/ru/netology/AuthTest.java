f3d28d6af7cdda1bd07885471a03def05794ed91
Dolgov David <dolgov.david.1993@gmail.com>
2023-07-03 09:33:56 +0300

    initial commit

---
+++ src/test/java/ru/netology/AuthTest.java
@@ -0,0 +1,74 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+
+class AuthTest {
+
+    @BeforeEach
+    void setUp() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+        DataName registration = DataGenerator.getNewUser("active");
+        $("[data-test-id=login] input").setValue(registration.getLogin());
+        $("[data-test-id=password] input").setValue(registration.getPassword());
+        $(".button").click();
+        $x("//h2[contains(text(), 'Личный кабинет')]").should(Condition.appear);
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        DataName registration = DataGenerator.getNewUser("blocked");
+        $("[data-test-id=login] input").setValue(registration.getLogin());
+        $("[data-test-id=password] input").setValue(registration.getPassword());
+        $(".button").click();
+        $(".notification__content").shouldHave(Condition.exactText("Ошибка! " + "Пользователь заблокирован"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        DataName registration = DataGenerator.getNewUser("active");
+        Faker faker = new Faker();
+        $("[data-test-id=login] input").setValue(faker.name().firstName());
+        $("[data-test-id=password] input").setValue(faker.internet().password());
+        $(".button").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.exactText("Ошибка! " + "Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        DataName registration = DataGenerator.getNewUser("active");
+        Faker faker = new Faker();
+        $("[data-test-id=login] input").setValue(faker.name().firstName());
+        $("[data-test-id=password] input").setValue(registration.getPassword());
+        $(".button").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.exactText("Ошибка! " + "Неверно указан логин или пароль"));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        DataName registration = DataGenerator.getNewUser("active");
+        Faker faker = new Faker();
+        $("[data-test-id=login] input").setValue(registration.getLogin());
+        $("[data-test-id=password] input").setValue(faker.internet().password());
+        $(".button").click();
+        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.exactText("Ошибка! " + "Неверно указан логин или пароль"));
+
+    }
+}
---
+++ src/test/java/ru/netology/DataGenerator.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.filter.log.LogDetail;
+
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public class DataGenerator {
+
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static DataName getNewUser(String status) {
+        Faker faker = new Faker(new Locale("en"));
+
+        DataName registration = new DataName(faker.name().firstName(), faker.internet().password(), status);
+        given()
+                .spec(requestSpec)
+                .body(registration)
+                .when()
+                .post("/api/system/users")
+                .then()
+                .statusCode(200);
+        return registration;
+    }
+}
